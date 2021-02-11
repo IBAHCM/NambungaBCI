dbb2827a8700c46ca17f97b1e49b82607ba9705b
Ismail Nambunga <ismailnambunga@Ismails-MacBook-Pro.local>
2021-02-11 18:38:02 +0000

    updated commits

---
+++ R/NambungaBCI-package.R
@@ -0,0 +1,16 @@
+#' NambungaBCI
+#' 
+#' \code{NambungaBCI} is a dummy package developed as part of the Reproducible
+#' Programming in R course..
+#' 
+#' This text will appear in the Details section of the documentation.
+#' 
+#' @author 
+#' Ismail Nambunga <2590916n@student.gla.ac.uk> (maintainer) \cr
+#' 
+#' @name NambungaBCI-package
+#' @aliases NambungaBCI
+#' @docType package
+#' 
+#' NULL
+#' 
